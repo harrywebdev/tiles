96b9699b59fb4b769e52f1a6bda3c6afa68334f4
Harry B <harry@gethornet.com>
2024-07-19 18:40:39 +0200

    add Edit switch

--- src/components/TopNavBar.tsx
+++ src/components/TopNavBar.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Container, Form, Navbar } from "react-bootstrap";
 
 type TopNavBarProps = {
   //
@@ -7,29 +7,13 @@ type TopNavBarProps = {
 
 const TopNavBar: FC<TopNavBarProps> = () => {
   return (
-    <Container>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </Container>
+    <Navbar bg="light" expand="lg">
+      <Container className={"justify-content-end"}>
+        <Form>
+          <Form.Check type="switch" id="top-nav-bar-switch-edit" label="Edit" />
+        </Form>
+      </Container>
+    </Navbar>
   );
 };
 
