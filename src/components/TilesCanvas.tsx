c0e5c00c6d8597e728d7c0b9413487b7402cbb9f
Harry B <harry@gethornet.com>
2024-07-19 18:56:48 +0200

    change bg when isEditing

--- src/components/TilesCanvas.tsx
+++ src/components/TilesCanvas.tsx
@@ -1,13 +1,17 @@
 import { FC } from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import { useAppEditStore } from "../stores/app-edit-store.ts";
 
 type TilesCanvasProps = {
   //
 };
 
 const TilesCanvas: FC<TilesCanvasProps> = () => {
+  const { isEditing } = useAppEditStore();
   return (
-    <div className={"flex-grow-1 d-flex"}>
+    <div
+      className={`flex-grow-1 d-flex ${isEditing ? "bg-white" : "bg-light"}`}
+    >
       <Container>
         <Row>
           <Col>MainScreen</Col>
